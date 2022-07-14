c24afcd5c4d912fa9045ce67d831151fdf8acaee
Cameron <c-em@outlook.com>
2022-07-14 14:01:36 -0700

    Added logging function

--- Flashcards/task/src/flashcards/Main.kt
+++ Flashcards/task/src/flashcards/Main.kt
@@ -3,101 +3,108 @@ package flashcards
 import java.io.File
 
 val flashcardPack = mutableListOf<Flashcard>()
+val logText = mutableListOf<String>()
 
 fun main(args: Array<String>) {
     if (args.contains("-import")) {
         import(args[args.indexOf("-import") + 1])
     }
     while (true) {
-        println("Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):")
-        when (readln()) {
+        val menuText = "Input the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):"
+        println(menuText)
+        logText.add(menuText)
+        val input = readln()
+        logText.add(input)
+        when (input) {
             "add" -> add()
             "remove" -> remove()
-            "import" -> import()
-            "export" -> export()
+            "import" -> import(fileNamePrompt())
+            "export" -> export(fileNamePrompt())
             "ask" -> ask()
             "exit" -> {
-                println("Bye bye!")
+                val exitMessage = "Bye bye!"
+                println(exitMessage)
+                logText.add(exitMessage)
                 if (args.contains("-export")) {
                     export(args[args.indexOf("-export") + 1])
                 }
                 break
             }
-            "log" -> log()
+            "log" -> log(fileNamePrompt())
             "hardest card" -> hardestCard()
             "reset stats" -> resetStats()
-            else -> println("Invalid entry")
+            else -> {
+                val errorMessage = "Invalid entry"
+                println(errorMessage)
+                logText.add(errorMessage)
+            }
         }
     }
-}
-
-fun resetStats() {
-    flashcardPack.forEach { it.mistakes = 0 }
-    println("Card statistics have been reset.")
-}
-
-fun hardestCard() {
-    var mostMistakes = 0
-    flashcardPack.forEach {
-        if (it.mistakes > mostMistakes) {
-            mostMistakes = it.mistakes
-        }
-    }
-    if (mostMistakes == 0) {
-        println("There are no cards with errors")
-        return
-    }
-    val hardestCards = flashcardPack.filter { it.mistakes == mostMistakes }
-    println("The hardest card"
-            + (if (hardestCards.size > 1) "s are " else " is ")
-            + hardestCards.joinToString { "\"${it.term}\"" }
-            + "."
-            + " You have $mostMistakes error${if (mostMistakes > 1) "s" else ""} answering "
-            + if (hardestCards.size > 1) "them." else "it."
-    )
-}
-
-fun log() {
-    TODO("Not yet implemented")
 }
 
 fun add() {
-    println("The card:")
+    val cardMessage = "The card:"
+    println(cardMessage)
+    logText.add(cardMessage)
     val term = readln()
+    logText.add(term)
     if (flashcardPack.any { it.term == term }) {
-        println("The card \"$term\" already exists.")
+        val termErrorMessage = "The card \"$term\" already exists."
+        println(termErrorMessage)
+        logText.add(termErrorMessage)
         return
     } else if (term.isBlank()) {
-        println("The card cannot be blank")
+        val termBlankMessage = "The card cannot be blank"
+        println(termBlankMessage)
+        logText.add(termBlankMessage)
         return
     }
-    println("The definition of the card:")
+    val definitionMessage = "The definition of the card:"
+    println(definitionMessage)
+    logText.add(definitionMessage)
     val definition = readln()
+    logText.add(definition)
     if (flashcardPack.any { it.definition == definition }) {
-        println("The definition \"$definition\" already exists:")
+        val definitionErrorMessage = "The definition \"$definition\" already exists:"
+        println(definitionErrorMessage)
+        logText.add(definitionErrorMessage)
         return
     } else if (definition.isBlank()) {
-        println("The definition cannot be blank")
+        val definitionBlankMessage = "The definition cannot be blank"
+        println(definitionBlankMessage)
+        logText.add(definitionBlankMessage)
         return
     }
     flashcardPack.add(Flashcard(term, definition, 0))
-    println("The pair (\"$term\":\"$definition\") has been added.")
+    val pairAddedMessage = "The pair (\"$term\":\"$definition\") has been added."
+    println(pairAddedMessage)
+    logText.add(pairAddedMessage)
 }
 
 fun remove() {
-    println("Which card?")
+    val cardMessage = "Which card?"
+    println(cardMessage)
+    logText.add(cardMessage)
     val cardToRemove = readln()
+    logText.add(cardToRemove)
     if (flashcardPack.removeIf { it.term == cardToRemove } || flashcardPack.removeIf { it.definition == cardToRemove }) {
-        println("The card has been removed")
+        val cardRemovedMessage = "The card has been removed"
+        println(cardRemovedMessage)
+        logText.add(cardRemovedMessage)
     } else {
-        println("Can't remove \"$cardToRemove\": there is no such card.")
+        val cardRemovedError = "Can't remove \"$cardToRemove\": there is no such card."
+        println(cardRemovedError)
+        logText.add(cardRemovedError)
     }
 }
 
-fun import() {
-    println("File name:")
-    val fileName = readln()
-    import(fileName)
+fun fileNamePrompt() : String {
+    val filePrompt = "File name:"
+    println(filePrompt)
+    logText.add(filePrompt)
+    val input = readln()
+    logText.add(input)
+    return input
 }
 
 fun import(fileName: String) {
@@ -112,16 +119,14 @@ fun import(fileName: String) {
                 count++
             }
         }
-        println("$count cards have been loaded.")
+        val cardsLoadedMessage = "$count cards have been loaded."
+        println(cardsLoadedMessage)
+        logText.add(cardsLoadedMessage)
     } catch (e: java.lang.Exception) {
-        println("File not found.")
+        val fileNotFoundMessage = "File not found."
+        println(fileNotFoundMessage)
+        logText.add(fileNotFoundMessage)
     }
-}
-
-fun export() {
-    println("File name:")
-    val fileName = readln()
-    export(fileName)
 }
 
 fun export(fileName: String) {
@@ -134,40 +139,104 @@ fun export(fileName: String) {
         flashcardPack.forEach {
             saveFile.appendText("${it.term} :: ${it.definition} :: ${it.mistakes}\n")
         }
-        println("${flashcardPack.size} cards have been saved.")
+        val cardsSavedMessage = "${flashcardPack.size} cards have been saved."
+        println(cardsSavedMessage)
+        logText.add(cardsSavedMessage)
     } catch (e: java.lang.Exception) {
-        println("Write Error")
+        val writeErrorMessage = "Write Error"
+        println(writeErrorMessage)
+        logText.add(writeErrorMessage)
     }
 }
 
 fun ask() {
     if (flashcardPack.isEmpty()) {
-        println("There are no flashcards available")
+        val noFlashcardsErrorMessage = "There are no flashcards available"
+        println(noFlashcardsErrorMessage)
+        logText.add(noFlashcardsErrorMessage)
         return
     }
-    println("How many times to ask?")
-    val timesToAsk = readln().toIntOrNull()
+    val askAmountPrompt = "How many times to ask?"
+    println(askAmountPrompt)
+    logText.add(askAmountPrompt)
+    val input = readln()
+    logText.add(input)
+    val timesToAsk = input.toIntOrNull()
     if (timesToAsk == null) {
-        println("Invalid Entry")
+        val invalidEntryErrorMessage = "Invalid Entry"
+        println(invalidEntryErrorMessage)
+        logText.add(invalidEntryErrorMessage)
         return
     }
     for (i in 0 until timesToAsk) {
         val randomCard = flashcardPack.random()
-        println("Print the definition of \"${randomCard.term}\":")
+        val askForDefinitionPrompt = "Print the definition of \"${randomCard.term}\":"
+        println(askForDefinitionPrompt)
+        logText.add(askForDefinitionPrompt)
         val answer = readln()
+        logText.add(answer)
         if (answer == randomCard.definition) {
-            println("Correct!")
+            val correctAnswerMessage = "Correct!"
+            println(correctAnswerMessage)
+            logText.add(correctAnswerMessage)
         }
         else {
-            print("Wrong. The right answer is \"${randomCard.definition}\"")
+            var wrongAnswerMessage = "Wrong. The right answer is \"${randomCard.definition}\""
             val correctCard: Flashcard? = flashcardPack.firstOrNull { it.definition == answer }
-            if (correctCard != null) {
-                println(", but your definition is correct for \"${correctCard.term}\".")
+            wrongAnswerMessage += if (correctCard != null) {
+                ", but your definition is correct for \"${correctCard.term}\"."
             } else {
-                println(".")
+                "."
             }
+            println(wrongAnswerMessage)
+            logText.add(wrongAnswerMessage)
             randomCard.mistakes++
         }
     }
+}
+
+fun log(fileName: String) {
+    try {
+        val logFile = File(fileName)
+        val logSavedMessage = "The log has been saved."
+        println(logSavedMessage)
+        logText.add(logSavedMessage)
+        logText.forEach { logFile.appendText(it + "\n") }
+    } catch (e: java.lang.Exception) {
+        val writeErrorMessage = "Write Error"
+        println(writeErrorMessage)
+        logText.add(writeErrorMessage)
+    }
+}
+
+fun hardestCard() {
+    var mostMistakes = 0
+    flashcardPack.forEach {
+        if (it.mistakes > mostMistakes) {
+            mostMistakes = it.mistakes
+        }
+    }
+    if (mostMistakes == 0) {
+        val noErrorsMessage = "There are no cards with errors"
+        println(noErrorsMessage)
+        logText.add(noErrorsMessage)
+        return
+    }
+    val hardestCards = flashcardPack.filter { it.mistakes == mostMistakes }
+    val hardestCardsMessage = ("The hardest card"
+            + (if (hardestCards.size > 1) "s are " else " is ")
+            + hardestCards.joinToString { "\"${it.term}\"" }
+            + "."
+            + " You have $mostMistakes error${if (mostMistakes > 1) "s" else ""} answering "
+            + if (hardestCards.size > 1) "them." else "it.")
+    println(hardestCardsMessage)
+    logText.add(hardestCardsMessage)
+}
+
+fun resetStats() {
+    flashcardPack.forEach { it.mistakes = 0 }
+    val resetMessage = "Card statistics have been reset."
+    println(resetMessage)
+    logText.add(resetMessage)
 }
 
